f828f872634a2fee66e86fc2c94322ea90489f4f
Michael S. Kenniston <git@michaelkenniston.com>
2026-07-26 22:49:12 -0400

    added more material to the TEECLI Manual

--- targets/Python/Environment.py
+++ targets/Python/Environment.py
@@ -3,6 +3,7 @@
 # TEECLI - Test Execution Environment for Cross-Language Implementations
 # Copyright (c) 2026 Michael S. Kenniston
 # Open-source licensed under LGPL 2.1.  See the LICENSE file for details.
+
 # Environments match values with variables (symbols), using lexical scoping.
 
 class Environment:
